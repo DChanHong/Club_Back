48af2b4788432f6bfb4c6d675b82e712398bf789
hong <bkn367@naver.com>
2023-06-02 19:21:34 +0900

    test commit

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser()); //쿠키을 사용할 것이기에 사용
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World!  commit 됐는가???/");
 });
 
 // router에 있는 customerRouter에서 customer DB를 잘 보내줄 예정이다.
