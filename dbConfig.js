4b72a49a14cc7761b4f1d5b20ed4709f832ecf7b
hong <bkn367@naver.com>
2023-07-08 16:22:35 +0900

    fix : ec2 mysql 테스트 , ec2 db api 요청 실패 해결 시도 , Fixes:이슈 수정중 1

--- dbConfig.js
+++ dbConfig.js
@@ -1,20 +1,33 @@
-const fs =require('fs');
+const fs = require("fs");
 
-const data =fs.readFileSync('./database.json');
+const data = fs.readFileSync("./database.json");
 const conf = JSON.parse(data);
-const mysql =require('mysql');
+const mysql = require("mysql");
 
 //굳이 파싱을 통해서 conf에 담아서 하는 이유는 보안을 위해서이다.
-const connection =mysql.createConnection({
-    host:conf.host,
-    user:conf.user,
-    password: conf.password,
-    port:conf.port,
-    database:conf.database
-})
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+});
+// connection.connect(function (err) {
+//   if (err) {
+//     console.error("Error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("Connected as id " + connection.threadId);
+// });
 
 // mysql과 연동이 완료
-connection.connect();
+connection.connect((error) => {
+  if (error) {
+    console.log("MYSQL CONNECTERROR" + error.stack);
+    return;
+  }
+  console.log("MTSQL CONNECT COMPLETE");
+});
 
 //이 connection 어디서든 쓸수 있게 export
-module.exports =connection;
+module.exports = connection;
